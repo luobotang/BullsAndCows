716d5e16b3033c6194194465eb724fe62888d23d
luobotang <tangkangxing@sina.com>
2015-08-27 12:46:07 +0800

    add msg

--- build.js
+++ build.js
@@ -4,7 +4,11 @@ var fs = require('fs')
 var input = './lib/expose.js'
 var output = 'BullsAndCows.js'
 
+var fail
+
 browserify()
+	.on('error', function (err) { faile = true; console.log(err) })
+	.on('bundle', function () { if (!fail) console.log('done! ' + output) })
 	.add(input)
 	.bundle()
 	.pipe(fs.createWriteStream(output))
