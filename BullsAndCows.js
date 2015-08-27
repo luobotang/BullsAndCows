698975419fb0716c699e79b74539f5c90d3941b5
luobotang <tangkangxing@sina.com>
2015-08-27 12:39:44 +0800

    build with browserify directly

--- BullsAndCows.js
+++ BullsAndCows.js
@@ -1,6 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-window.Game = require('./lib/game.js')
-},{"./lib/game.js":2}],2:[function(require,module,exports){
+window.Game = require('./game.js')
+},{"./game.js":2}],2:[function(require,module,exports){
 // <猜数字游戏>
 // 标准玩法
 //     给出4个不重复的数字（[0-9]），猜的人猜4个数字，出数字
--- build.js
+++ build.js
@@ -1,1 +1,10 @@
-window.Game = require('./lib/game.js')
+var browserify = require('browserify')
+var fs = require('fs')
+
+var input = './lib/expose.js'
+var output = 'BullsAndCows.js'
+
+browserify()
+	.add(input)
+	.bundle()
+	.pipe(fs.createWriteStream(output))
